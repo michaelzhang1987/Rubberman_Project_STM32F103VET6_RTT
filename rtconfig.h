8405e61ffc35ada3591026845f0f0e465c98fb80
geek_michael <1069513045@qq.com>
2021-03-27 21:26:04 +0800

    2021.03.27 添加软件包FAL,测试基本通过，为后续添加easyflash软件包做准备

--- rtconfig.h
+++ rtconfig.h
@@ -167,6 +167,12 @@
 
 /* system packages */
 
+#define PKG_USING_FAL
+#define FAL_DEBUG_CONFIG
+#define FAL_DEBUG 1
+#define FAL_PART_HAS_TABLE_CFG
+#define PKG_USING_FAL_V00500
+#define PKG_FAL_VER_NUM 0x00500
 
 /* Micrium: Micrium software products porting for RT-Thread */
 
@@ -201,6 +207,7 @@
 #define BSP_USING_GPIO
 #define BSP_USING_UART
 #define BSP_USING_UART1
+#define BSP_USING_ON_CHIP_FLASH
 #define BSP_USING_SPI
 #define BSP_USING_SPI1
 
